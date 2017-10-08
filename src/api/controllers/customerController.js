500475dc814b91ff0f2f60f0a48ca8437681d532
adhe223 <adhe223@g.uky.edu>
2017-10-08 09:54:07 -0700

    Add hours text. Complete more customer api methods

--- src/api/controllers/customerController.js
+++ src/api/controllers/customerController.js
@@ -1,21 +1,46 @@
 const db = require('../../db');
 
 const getAllCustomers = (req, res) => {
-  db.query('SELECT * FROM customer', (err, results, fields) => {
+  db.query('SELECT * FROM customer', (err, result, fields) => {
     if (err) {
       res.send(err);
     };
 
-    res.json(results);
+    res.json(result);
   });
 };
 
 const createCustomer = (req, res) => {
+  const customer = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    companyName: req.body.companyName,
+    streetAddress: req.body.streetAddress,
+    streetAddress2: req.body.streetAddress2,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    phoneNumber: req.body.phoneNumber,
+  };
 
+  dq.query('INSERT INTO customer SET ?', customer, (err, result) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log('successfully inserted customer');
+    }
+  });
 };
 
 const getCustomer = (req, res) => {
-
+  const customerId = req.body.customerId;
+  db.query('SELECT * FROM customer WHERE id = ?', [customerId], (err, result) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      res.json(result);
+    }
+  });
 };
 
 const updateCustomer = (req, res) => {
